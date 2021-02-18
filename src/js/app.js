c18f309d42fc4a722201bc764540aad571fe56ab
Pezu-git <artem_nikolaev69@mail.ru>
2021-02-18 19:04:44 +0300

    eslint add

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
-export default function mage(option) {
-  for (let item of option) {
+/* eslint-disable no-restricted-syntax */
+export default function Mage(option) {
+  for (const item of option) {
     if (item.health <= 15) {
-      return "critical";
-    } else if (item.health <= 50) {
-      return "wounded";
-    } else {
-      return "healthy";
+      return 'critical';
     }
+    if (item.health <= 50) {
+      return 'wounded';
+    }
+    return 'healthy';
   }
 }
